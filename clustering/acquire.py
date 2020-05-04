51f1f79e58348197e2d3c89327b889062cd13425
Chase Thompson <chase.w.thompson210@gmail.com>
2020-05-04 14:19:19 -0500

    updated, and added new py file

--- clustering/acquire.py
+++ clustering/acquire.py
@@ -1,120 +1,52 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np 
-from env import host, user, password
+import numpy as np
+import env
 
-# function to contact database
+def get_db_url(database):
+    from env import host, user, password
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    return url
 
-def get_db_url(db_name):
-    return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
+def get_zillow_data():
+    query = """
+    SELECT * FROM properties_2017
+    RIGHT JOIN `predictions_2017` USING (`parcelid`)
+    LEFT JOIN `architecturalstyletype` USING (`architecturalstyletypeid`) 
+    LEFT JOIN `buildingclasstype` USING (`buildingclasstypeid`)
+    LEFT JOIN `heatingorsystemtype` USING (`heatingorsystemtypeid`)
+    LEFT JOIN `propertylandusetype` USING (`propertylandusetypeid`)
+    LEFT JOIN `storytype` USING (`storytypeid`)
+    LEFT JOIN `airconditioningtype` USING (`airconditioningtypeid`)
+    LEFT JOIN `typeconstructiontype` USING (`typeconstructiontypeid`) 
+    WHERE properties_2017.latitude IS NOT NULL OR  properties_2017.longitude IS NOT NULL
+    """
+    df = pd.read_sql(query, get_db_url('zillow'))
+    # Drop one of the duplicate column named id
+    df = pd.concat([
+    df.iloc[:, :11], # all the rows, and up to, but not including the index of the column to drop
+    df.iloc[:, 11 + 1:] # all the rows, and everything after the column to drop
+    ], axis=1)
+    #sort values by parcelid and transactiondate
+    df = df.sort_values(["parcelid", "transactiondate"])
 
-# function to query db and return df
-
-def wrangle_zillow():
-    url = get_db_url('zillow')
-    query = '''
-    SELECT prop.*, 
-       pred1.logerror, 
-       pred1.transactiondate, 
-       air.airconditioningdesc, 
-       arch.architecturalstyledesc, 
-       build.buildingclassdesc, 
-       heat.heatingorsystemdesc, 
-       landuse.propertylandusedesc, 
-       story.storydesc, 
-       construct.typeconstructiondesc 
-FROM   properties_2017 prop 
-       LEFT JOIN predictions_2017 pred1 USING (parcelid) 
-       INNER JOIN (SELECT parcelid, 
-                          Max(transactiondate) maxtransactiondate 
-                   FROM   predictions_2017 
-                   GROUP  BY parcelid) pred2 
-               ON pred1.parcelid = pred2.parcelid 
-                  AND pred1.transactiondate = pred2.maxtransactiondate 
-       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
-       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
-       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
-       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
-       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
-       LEFT JOIN storytype story USING (storytypeid) 
-       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
-WHERE  prop.latitude IS NOT NULL 
-	   AND prop.longitude IS NOT NULL; 
-    '''
-
-    zillow = pd.read_sql(query,url)
-    return zillow
-
-def measure_na_columns(df):
-    na_column_df = pd.DataFrame(df.isna().sum(), columns=['num_na_rows'])
-    na_column_df['pct_na_rows'] = df.isna().sum() / len(df.index)
-    return na_column_df
-
-def measure_na_rows(df):
-    na_row_df = pd.DataFrame(df.isna().sum(axis=1).value_counts(sort=False), 
-                      columns=['num_rows'])
-    na_row_df = na_row_df.reset_index()
-    na_row_df = na_row_df.rename(columns={'index': 'num_col_missing'})
-    na_row_df['pct_cols_missing'] = na_row_df.num_col_missing / len(df.columns.tolist())
-    return na_row_df
-
-def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
-    threshold = int(round(prop_required_column*len(df.index),0))
-    df.dropna(axis=1, thresh=threshold, inplace=True)
-    threshold = int(round(prop_required_row*len(df.columns),0))
-    df.dropna(axis=0, thresh=threshold, inplace=True)
+    # drop duplicates
+    df = df.drop_duplicates(subset=["parcelid"],keep='last')
     return df
 
-def remove_columns(df, cols_to_remove):  
-    df = df.drop(columns=cols_to_remove)
-    return df
+def rows_missing(df):
+    df1 = pd.DataFrame(df.isnull().sum(), columns = ['num_rows_missing'])
+    df1['pct_rows_missing'] = df1.num_rows_missing/df.shape[0]
+    return df1  
 
-def label_county(row):
-    if row['fips'] == 6037:
-        return 'Los Angeles'
-    elif row['fips'] == 6059:
-        return 'Orange'
-    elif row['fips'] == 6111:
-        return 'Ventura'
-
-def prep_zillow(cols_to_remove=['calculatedbathnbr', 'heatingorsystemtypeid', 'regionidneighborhood'], prop_required_column=.5, prop_required_row=.75):
-    zillow = wrangle_zillow()
-    
-    #insure that this is a single unit property
-    zillow = zillow[zillow.propertylandusetypeid
-                             .isin([261, 262, 263, 264, 266, 268, 273, 276, 279])
-                            ]
-    zillow = zillow[(zillow.bedroomcnt > 0) & (zillow.bathroomcnt > 0)]
-    zillow = remove_columns(zillow, cols_to_remove)
-    zillow = handle_missing_values(zillow, prop_required_column, prop_required_row)
-    
-    # Set nulls to specific value
-    zillow.calculatedfinishedsquarefeet[zillow.calculatedfinishedsquarefeet.isna()] = zillow.calculatedfinishedsquarefeet.median()
-    
-    zillow.buildingqualitytypeid = zillow.buildingqualitytypeid.fillna(6)
-    zillow.finishedsquarefeet12 = zillow.finishedsquarefeet12.fillna(1456.0)
-    
-    zillow.lotsizesquarefeet = zillow.lotsizesquarefeet.fillna(zillow.lotsizesquarefeet.median())
-    
-    zillow = zillow.dropna(subset=['regionidcity', 'regionidzip', 'censustractandblock'])
-    
-    zillow.propertyzoningdesc = zillow.propertyzoningdesc.fillna('LAR1')
-    
-    zillow.heatingorsystemdesc = zillow.propertyzoningdesc.fillna('N/A')
-    
-    zillow.structuretaxvaluedollarcnt = zillow.structuretaxvaluedollarcnt.fillna(zillow.structuretaxvaluedollarcnt.median())
-    
-    zillow.yearbuilt = zillow.yearbuilt.fillna(zillow.yearbuilt.median())
-    
-    zillow.taxamount = zillow.taxamount.fillna(zillow.taxamount.median())
-    
-    zillow.unitcnt = zillow.unitcnt.fillna(1.0)
-    
-    zillow['County'] = zillow.apply(lambda row: label_county(row), axis=1)
-
-    zillow['State'] = 'CA'
-    
-    zillow = zillow.dropna(axis=1)
-    
-    return zillow
+def cols_missing(df):
+    """function that takes in a dataframe of observations and attributes and returns a dataframe where each row is
+    an atttribute name, the first column is the number of rows with missing values for that attribute, 
+    and the second column is percent of total rows that have missing values for that attribute"""
+    df['total'] = df.apply(lambda row: row.isnull().sum(), axis =1)
+    df2 = df[['total']]
+    df2 = df2.reset_index()
+    df2 = df2.groupby('total').count()
+    df2= df2.reset_index()
+    df2.rename(columns = {'total': 'num_cols_missing', 'index': 'num_rows'}, inplace = True)
+    df2['pct_cols_missing'] = df2.num_cols_missing/df.shape[1]
+    return df2
